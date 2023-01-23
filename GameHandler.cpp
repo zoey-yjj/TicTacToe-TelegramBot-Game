be7b942ae42f32ac685565e1be98c16af100143f
zoey-yjj <zoeyyyjj@gmail.com>
2023-01-23 12:26:26 +0800

    add message handler

--- GameHandler.cpp
+++ GameHandler.cpp
@@ -1,2 +1,15 @@
 #include "GameHandler.h"
 
+GameHandler::GameHandler(TgBot::Bot& bot) : bot(bot), board(3, std::vector<char>(3, ' ')), currentPlayer('X') {}
+
+void GameHandler::handleIncomingMessage(TgBot::Message::Ptr message) {
+    // Check if it's a text message
+    if (message->text.empty())
+        return;
+
+    // Check if it's a command to start the game
+    if (message->text == "/startgame") {
+        // Reset the game board
+        bot.getApi().sendMessage(message->chat->id, "Game is started!!");
+    }
+}
--- GameHandler.h
+++ GameHandler.h
@@ -3,5 +3,12 @@
 #include <tgbot/tgbot.h>
 
 class GameHandler {
+public:
+    GameHandler(TgBot::Bot& bot);
+    void handleIncomingMessage(TgBot::Message::Ptr message);
 
+private:
+    TgBot::Bot& bot;
+    std::vector<std::vector<char>> board;
+    char currentPlayer;
 };
